38770183b7234e18bd778d3a34c1edcdc184bce4
SaifN97 <saifnarpali@gmail.com>
2021-06-06 19:35:22 +0530

    Added pagination

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         $user = User::factory()->create([
             'name' => 'John Doe'
         ]);
-        Post::factory(10)->create([
+        Post::factory(30)->create([
             'user_id' => $user->id
         ]);
     }
